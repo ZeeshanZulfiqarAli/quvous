f11d26fbfc05e18fa23d8f8f242c199d6268f6ab
Zeeshan <zeeshan.dossani23@gmail.com>
2022-10-16 15:02:44 +0500

    Fix name

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -44,7 +44,7 @@ const Header = () => {
               textDecoration: "none",
             }}
           >
-            CUVIOUS
+            QUVOUS
           </Typography>
 
           {isLoggedIn ? (
